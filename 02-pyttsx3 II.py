8df34d3683c47c5e74087ea8c440aa0fb492b3c3
Rodrigo <rcbm539@gmail.com>
2022-09-27 17:20:05 -0300

    01:02 - Utilizando Pyttsx3 com Input

--- 02-pyttsx3 II.py
+++ 02-pyttsx3 II.py
@@ -2,6 +2,6 @@ import pyttsx3
 
 engine = pyttsx3.init()
 engine.setProperty("voice", "brazil")
-frase = input("Digite a frase a ser falada\n")
+frase = input("Digite a frase\n")
 engine.say(frase)
 engine.runAndWait()
